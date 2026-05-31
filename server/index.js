f863af6668709df7b2930f5aa433df4ae32d6e75
adityabh18 <adityabhardwaj3112001@gmail.com>
2026-05-31 10:55:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"https://exam-notes-9riz.onrender.com",
         credentials:true,
         methods:["GET","POST","PUT","DELETE","OPTIONS"]
     }
